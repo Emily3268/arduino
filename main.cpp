db56a2005e2921d60e53312c2c2792bc41e7ae1f
Emily3268 <emily.pereiras2008@gmail.com>
2026-05-05 22:29:12 -0300

    Fix variable initialization and print sum in loop

---
+++ main.cpp
@@ -0,0 +1,62 @@
+int num1 = 60;
+int num2 = 120;
+soma = 0;
+
+
+
+
+void setup() { 9600
+  // put your setup code here, to run once:
+
+}
+
+void loop() {  
+soma = num1 + num2;
+
+}
+
+
+
+
+
+int num1 = 60;
+int num2 = 120;
+soma = 0;
+
+
+
+
+void setup() { Serial.begin (9600)
+  // put your setup code here, to run once:
+
+}
+
+void loop() {  
+soma = num1 + num2;
+Serial.println(200);
+
+}
+
+
+
+
+
+
+int num1 = 60;
+int num2 = 120;
+int soma = 0;
+
+
+
+
+void setup() { Serial.begin (9600);
+  // put your setup code here, to run once:
+
+}
+
+void loop() {  
+
+Serial.println(soma = num1 + num2);
+
+}
+
